4eb90c6b66cf0ed6230b8ed8a6fb4d158d98eacd
Hyungmin <hmjeon@mit.edu>
2019-04-06 01:53:36 -0400

    complete cal

--- prac/cal/modules/main.py
+++ prac/cal/modules/main.py
@@ -1,47 +1,60 @@
 # -*- coding: utf-8 -*-
 
-import sys
+import sys, ui
 
 import PyQt5
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
+'''
 from PyQt5 import uic
 
 calUI = '../_uiFiles/calculator.ui'
+'''
 
-class MainDialog(QDialog):
+class MainDialog(QDialog, ui.Ui_Dialog):
     def __init__(self):
         QDialog.__init__(self, None)
-        uic.loadUi(calUI, self)
+        #uic.loadUi(calUI, self)
+        self.setupUi(self)
 
-        self.num_pushButton_1.clicked.connect(lambda state, button = self.num_pushButton_1 : self.Clicked_Buttom(state, button))
-        self.num_pushButton_2.clicked.connect(lambda state, button = self.num_pushButton_2 : self.Clicked_Buttom(state, button))
-        self.num_pushButton_3.clicked.connect(lambda state, button = self.num_pushButton_3 : self.Clicked_Buttom(state, button))
-        self.num_pushButton_4.clicked.connect(lambda state, button = self.num_pushButton_4 : self.Clicked_Buttom(state, button))
-        self.num_pushButton_5.clicked.connect(lambda state, button = self.num_pushButton_5 : self.Clicked_Buttom(state, button))
-        self.num_pushButton_6.clicked.connect(lambda state, button = self.num_pushButton_6 : self.Clicked_Buttom(state, button))
-        self.num_pushButton_7.clicked.connect(lambda state, button = self.num_pushButton_7 : self.Clicked_Buttom(state, button))
-        self.num_pushButton_8.clicked.connect(lambda state, button = self.num_pushButton_8 : self.Clicked_Buttom(state, button))
-        self.num_pushButton_9.clicked.connect(lambda state, button = self.num_pushButton_9 : self.Clicked_Buttom(state, button))
-        self.num_pushButton_0.clicked.connect(lambda state, button = self.num_pushButton_0 : self.Clicked_Buttom(state, button))
+        # Initialize the editor as zero
+        self.a_lineEdit.setText("0")
 
-        self.sign_pushButton_1.clicked.connect(lambda state, button = self.sign_pushButton_1 : self.Clicked_Buttom(state, button))
-        self.sign_pushButton_2.clicked.connect(lambda state, button = self.sign_pushButton_2 : self.Clicked_Buttom(state, button))
-        self.sign_pushButton_3.clicked.connect(lambda state, button = self.sign_pushButton_3 : self.Clicked_Buttom(state, button))
-        self.sign_pushButton_4.clicked.connect(lambda state, button = self.sign_pushButton_4 : self.Clicked_Buttom(state, button))
+        # Push button events for digits
+        self.num_pushButton_1.clicked.connect(self.Clicked_Buttom)
+        self.num_pushButton_2.clicked.connect(self.Clicked_Buttom)
+        self.num_pushButton_3.clicked.connect(self.Clicked_Buttom)
+        self.num_pushButton_4.clicked.connect(self.Clicked_Buttom)
+        self.num_pushButton_5.clicked.connect(self.Clicked_Buttom)
+        self.num_pushButton_6.clicked.connect(self.Clicked_Buttom)
+        self.num_pushButton_7.clicked.connect(self.Clicked_Buttom)
+        self.num_pushButton_8.clicked.connect(self.Clicked_Buttom)
+        self.num_pushButton_9.clicked.connect(self.Clicked_Buttom)
+        self.num_pushButton_0.clicked.connect(self.Clicked_Buttom)
 
+        # Push button events for signs
+        self.sign_pushButton_1.clicked.connect(self.Clicked_Buttom)
+        self.sign_pushButton_2.clicked.connect(self.Clicked_Buttom)
+        self.sign_pushButton_3.clicked.connect(self.Clicked_Buttom)
+        self.sign_pushButton_4.clicked.connect(self.Clicked_Buttom)
+
+        # Push button events for etc
+        self.p_open_pushButton.clicked.connect(self.Clicked_Buttom)
+        self.p_close_pushButton.clicked.connect(self.Clicked_Buttom)
+        self.dot_pushButton.clicked.connect(self.Clicked_Buttom)
+        self.per_pushButton.clicked.connect(self.Clicked_Buttom)
+
+        # Push button events for result, reset and deletion
         self.result_pushButton.clicked.connect(self.MakeResult)
         self.reset_pushButton.clicked.connect(self.Reset)
         self.del_pushButton.clicked.connect(self.Delete)
 
-        self.p_open_pushButton.clicked.connect(lambda state, button=self.p_open_pushButton: self.Clicked_Buttom(state, button))
-        self.p_close_pushButton.clicked.connect(lambda state, button=self.p_close_pushButton: self.Clicked_Buttom(state, button))
-        self.dot_pushButton.clicked.connect(lambda state, button=self.dot_pushButton: self.Clicked_Buttom(state, button))
-        self.per_pushButton.clicked.connect(lambda state, button=self.per_pushButton: self.Clicked_Buttom(state, button))
+    def Clicked_Buttom(self):
 
-    def Clicked_Buttom(self, state, button):
+        button = self.sender()
+
         if button == self.per_pushButton:
             now_num_text = '*0.01'
         else:
